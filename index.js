9544ffcc85bab1eabc455def53b4de43796daa23
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2022-12-15 10:54:28 +0800

    database

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-const fruit = ["Apple", "Banana"];
+const db = require("./db/models/index");
+const { fruit } = db;
 
 const FruitController = require("./controllers/FruitController");
-const fruitController = new FruitController();
+const fruitController = new FruitController(fruit);
 
 const FruitRouter = require("./routers/FruitRouter");
 const fruitRouter = new FruitRouter(fruitController, express);
@@ -12,6 +14,7 @@ const fruitRouter = new FruitRouter(fruitController, express);
 // Setting up middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/fruit", fruitRouter.route());
 
